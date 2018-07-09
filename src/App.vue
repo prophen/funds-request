cc3f8353fe84ae837ae399103b246593f7bca5c2
Nikema Prophet <nikema@gmail.com>
2018-07-09 14:46:21 -0700

    created Card component

--- src/App.vue
+++ src/App.vue
@@ -3,48 +3,25 @@
   <div id="app">
     <navigation></navigation>
     <b-container>
-
       <carousel></carousel>
-      <b-card title="Card Title"
-              img-src="https://picsum.photos/600/300/?image=25"
-              img-alt="Image"
-              img-top
-              tag="article"
-              style="max-width: 20rem;"
-              class="mb-2">
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </p>
-        <b-button href="#" variant="primary">Go somewhere</b-button>
-      </b-card>
+      <card></card>
     </b-container>
   </div>
+  
 </template>
 
 <script>
 import Navigation from './components/Navigation.vue'
 import Carousel from './components/Carousel.vue'
+import Card from './components/Card.vue'
 export default {
   components: {
     Navigation,
-    Carousel
+    Carousel,
+    Card
   },
   name: 'app',
-  data () {
-    return {
-      slide: 0,
-      sliding: null
-    }
-  },
-  methods: {
-    onSlideStart (slide) {
-      this.sliding = true
-    },
-    onSlideEnd (slide) {
-      this.sliding = false
-    },
-    
-  }
+
 }
 </script>
 
--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -53,3 +53,22 @@
 
   </div>
 </template>
+<script>
+export default {
+    data () {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart (slide) {
+      this.sliding = true
+    },
+    onSlideEnd (slide) {
+      this.sliding = false
+    },
+    
+  }
+}
+</script>
